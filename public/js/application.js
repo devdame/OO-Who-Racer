40c452c2495453bfbdea7f4766c89a34775bb9b2
Apprentice <apprentice@dbc-chicago-015-109.local>
2014-02-24 15:45:53 -0600

    working racer - pulling functions out for end/restart functionality

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,32 @@
 $(document).ready(function() {
   $(document).on('keyup', function(event) {
-      if(event.keyCode==81) {
-        $('#player1_strip').find('.active').removeClass("active").next().addClass("active");
-      };
-      if(event.keyCode==80) {
-        $('#player2_strip').find('.active').removeClass("active").next().addClass("active");
-      };
+    var strip1 = $('#player1_strip')
+    var strip2 = $('#player2_strip')
+
+    var player1 = strip1.find('.active')
+    var player2 = strip2.find('.active')
+
+    if(event.keyCode==81) {
+      $(player1).removeClass("active").next().addClass("active");
+      if(player1.next().hasClass("end")) {
+        $(document).unbind('keyup');
+        alert("Placeholder 1 is victorious!");
+    };
+    };
+    if(event.keyCode==80) {
+      $(player2).removeClass("active").next().addClass("active");
+      if(player2.next().hasClass("end")) {
+        $(document).unbind('keyup');
+        alert("Placeholder 2 is victorious!");
+    };
+    };
   });
 });
+
+function raceStop() {
+
+}
+
+
+
+strip1.removeClass("active").find('.start').addClass("active");
