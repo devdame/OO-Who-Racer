d0c00cf389e2c8d2a31e5d704ed08691e96588c6
Apprentice <apprentice@dbc-chicago-015-109.local>
2014-02-24 17:20:51 -0600

    no rebind on victory/reset - need to rebind

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  $('body').on("click", "#reset_button", function(e) {
+    resetRace();
+  });
   $(document).on('keyup', function(event) {
     var strip1 = $('#player1_strip')
     var strip2 = $('#player2_strip')
@@ -6,27 +9,32 @@ $(document).ready(function() {
     var player1 = strip1.find('.active')
     var player2 = strip2.find('.active')
 
-    if(event.keyCode==81) {
-      $(player1).removeClass("active").next().addClass("active");
-      if(player1.next().hasClass("end")) {
-        $(document).unbind('keyup');
-        alert("Placeholder 1 is victorious!");
-    };
-    };
-    if(event.keyCode==80) {
-      $(player2).removeClass("active").next().addClass("active");
-      if(player2.next().hasClass("end")) {
-        $(document).unbind('keyup');
-        alert("Placeholder 2 is victorious!");
-    };
-    };
+      if(event.keyCode==81) {
+        $(player1).removeClass("active").next().addClass("active");
+        if(player1.next().hasClass("end")) {
+          raceStop();
+          $("#victory1").css("display", "block");
+        };
+      };
+      if(event.keyCode==80) {
+        $(player2).removeClass("active").next().addClass("active");
+        if(player2.next().hasClass("end")) {
+          raceStop();
+          $("#victory2").css("display", "block");
+        };
+      };
   });
 });
 
 function raceStop() {
+  $(document).unbind('keyup');
+}
 
+function resetRace() {
+  $('.racer_table').find('.active').removeClass("active");
+  $('.racer_table').find('.start').addClass("active");
 }
 
 
 
-strip1.removeClass("active").find('.start').addClass("active");
+// strip1.removeClass("active").find('.start').addClass("active");
