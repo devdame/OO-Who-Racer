d4f18ddbe158cea47ec8fc13312588bbd8575f03
Apprentice <apprentice@dbc-chicago-015-109.local>
2014-02-24 20:53:00 -0600

    styled that shit up

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   resetRace();
   $('body').on("click", "#reset_button", function(e) {
+    var startTime = new Date().getTime();
     resetRace();
     $('#reset_button').text('RESET');
 
@@ -23,7 +24,14 @@ $(document).ready(function() {
       if(event.keyCode==80) {
         $(player2).removeClass("active").next().addClass("active");
         if(player2.next().hasClass("end")) {
+          var endTime = new Date().getTime();
+          var playTime = endTime - startTime;
+          var finalTime = playTime/1000;
+          $('#result').css("background", "blue");
+          $('#result').text("You finished in " + finalTime + " seconds");
           $(player2).next().css("background-image", "url('../tardis.png')");
+          $(player2).next().addClass("backlit");
+          $(strip2).find(".end").css("box-shadow", "0 0 50px gold,0 0 150px gold");
           raceStop();
           $("#victory2").css("display", "block");
         };
